5659772b38764377ca9bb2ba09dab4e5d74e4a5a
fabian <fabianrojasgamboa@gmail.com>
2024-11-08 18:45:55 -0300

    mejora de la interfaz parte 1

--- CompuMovil/lib/screens/login_screen.dart
+++ CompuMovil/lib/screens/login_screen.dart
@@ -4,7 +4,6 @@ import 'package:proyecto/screens/error_screen.dart';
 import 'package:proyecto/services/google_services.dart';
 import 'package:proyecto/services/rest_services.dart';
 import 'package:proyecto/widgets/Down_menu.dart';
-import 'package:proyecto/widgets/app_bar.dart';
 
 class LoginScreen extends StatelessWidget {
   static final Logger _logger = Logger();
@@ -14,48 +13,75 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const BarraApp(titulo: 'Pagina de Login'),
-      body: Center(
-        child: Padding(
-            padding: const EdgeInsets.all(100),
-            child: ElevatedButton(
-                onPressed: () {
-                  GoogleService.logIn().then((result) async {
-                    if (result) {
-                      _logger.i("Me autentifique");
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.white, Colors.deepPurpleAccent],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 90,
+            ),
+            Container(
+              width: 150,
+              height: 200,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('lib/images/signoUtem.png'),
+                    fit: BoxFit.cover),
+              ),
+            ),
+            Center(
+              child: Padding(
+                  padding: const EdgeInsets.all(100),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        GoogleService.logIn().then((result) async {
+                          if (result) {
+                            _logger.i("Me autentifique");
 
-                      // Llama al servicio después de autenticar
-                      try {
-                        await RestService.categories();
-                        await RestService.fetchAndSaveTypes();
-                        await RestService.fetchAndSaveStatuses();
+                            // Llama al servicio después de autenticar
+                            try {
+                              await RestService.categories();
+                              await RestService.fetchAndSaveTypes();
+                              await RestService.fetchAndSaveStatuses();
 
-                        // Redirige al DownMenu después de guardar los datos
-                        Navigator.push(
+                              // Redirige al DownMenu después de guardar los datos
+                              Navigator.push(
+                                  // ignore: use_build_context_synchronously
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => DownMenu()));
+                            } catch (e) {
+                              _logger.e("Error al consumir la API: $e");
+                              Navigator.push(
+                                  // ignore: use_build_context_synchronously
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const ErrorScreen()));
+                            }
+                          } else {
+                            _logger.i("fui bueno");
                             // ignore: use_build_context_synchronously
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => DownMenu()));
-                      } catch (e) {
-                        _logger.e("Error al consumir la API: $e");
-                        Navigator.push(
-                            // ignore: use_build_context_synchronously
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const ErrorScreen()));
-                      }
-                    } else {
-                      _logger.i("fui bueno");
-                      // ignore: use_build_context_synchronously
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return const ErrorScreen();
-                      }));
-                    }
-                  });
-                },
-                child: const Row(
-                    children: [Icon(Icons.g_mobiledata), Text('Login')]))),
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return const ErrorScreen();
+                            }));
+                          }
+                        });
+                      },
+                      child: const Row(children: [
+                        Icon(Icons.g_mobiledata),
+                        Text('Login')
+                      ]))),
+            ),
+          ],
+        ),
       ),
     );
   }
