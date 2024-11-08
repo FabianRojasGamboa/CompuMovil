d7eecfa2299c1c386cfa2a1a4a91a845ade29538
fabian <fabianrojasgamboa@gmail.com>
2024-11-08 14:53:51 -0300

    se agrego una refresh a la lista de tickets

--- CompuMovil/lib/screens/tickets_view_list.dart
+++ CompuMovil/lib/screens/tickets_view_list.dart
@@ -19,6 +19,12 @@ class _TicketListScreenState extends State<TicketListScreen> {
         AllTickets(); // Llama a la función para obtener los tickets
   }
 
+  Future<void> _refreshTickets() async {
+    setState(() {
+      _ticketsFuture = AllTickets(); // Recarga los tickets
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<Ticket>>(
@@ -32,83 +38,86 @@ class _TicketListScreenState extends State<TicketListScreen> {
           return const Center(child: Text('No se encontraron tickets.'));
         } else {
           final tickets = snapshot.data!;
-          return ListView.builder(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            itemCount: tickets.length,
-            itemBuilder: (context, index) {
-              final ticket = tickets[index];
-              return Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                child: ClipRRect(
-                  borderRadius:
-                      BorderRadius.circular(10), // Redondea los bordes
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 3,
-                          blurRadius: 8,
-                          offset: const Offset(0, 3),
-                        ),
-                      ],
-                    ),
-                    child: ExpansionTile(
-                      title: Text(
-                        ticket.subject,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
+          return RefreshIndicator(
+            onRefresh: _refreshTickets, // Añade la funcionalidad de refrescar
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              itemCount: tickets.length,
+              itemBuilder: (context, index) {
+                final ticket = tickets[index];
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: ClipRRect(
+                    borderRadius:
+                        BorderRadius.circular(10), // Redondea los bordes
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 3,
+                            blurRadius: 8,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
                       ),
-                      subtitle: Text(
-                        'Tipo: ${ticket.category.name}',
-                        style: TextStyle(color: Colors.grey[700]),
-                      ),
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              right: 20, left: 20, bottom: 10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              Text(
-                                'Status: ${ticket.status}',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.blueGrey[800],
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              const SizedBox(height: 5),
-                              Text(
-                                'Categoría: ${ticket.type}',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.teal[700],
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              const SizedBox(height: 5),
-                              Text(
-                                'Mensaje: ${ticket.message}',
-                                style: const TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.black87,
-                                  fontStyle: FontStyle.italic,
-                                ),
-                              ),
-                            ],
+                      child: ExpansionTile(
+                        title: Text(
+                          ticket.subject,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
                           ),
                         ),
-                      ],
+                        subtitle: Text(
+                          'Tipo: ${ticket.category.name}',
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                right: 20, left: 20, bottom: 10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Text(
+                                  'Status: ${ticket.status}',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.blueGrey[800],
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                  'Categoría: ${ticket.type}',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.teal[700],
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                  'Mensaje: ${ticket.message}',
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black87,
+                                    fontStyle: FontStyle.italic,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           );
         }
       },
